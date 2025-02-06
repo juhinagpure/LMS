43be669baf2b64691a6a9d672f45540b0aeeee9c
JUHI NAGPURE <juhinagpure143@gmail.com>
2025-02-06 22:31:24 +0530

    refactor routing in App component and update Educator page title

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -22,10 +22,10 @@ const App = () => {
         <Route path="/player/:courseId" element={<Player />} />
         <Route path="/loading/:path" element={<Loading />} />
         <Route path="/educator" element={<Educator />}>
-          <Route path="/educator" element={<Dashboard />} />
-          <Route path="/add-course" element={<AddCourse />} />
-          <Route path="/my-courses" element={<MyCourses />} />
-          <Route path="/students-enrolled" element={<StudentsEnrolled />} />
+          <Route path="educator" element={<Dashboard />} />
+          <Route path="add-course" element={<AddCourse />} />
+          <Route path="my-courses" element={<MyCourses />} />
+          <Route path="students-enrolled" element={<StudentsEnrolled />} />
         </Route>
       </Routes>
     </div>
--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -1,7 +1,9 @@
+import { Outlet } from "react-router-dom";
 const Educator = () => {
   return (
     <div>
-      <h1>Educator</h1>
+      <h1>Educator page</h1>
+      <div>{<Outlet />}</div>
     </div>
   );
 };
