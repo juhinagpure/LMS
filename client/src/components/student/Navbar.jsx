a5d3c5099a524a5e4f0e226d8025d1c18fe2a18b
JUHI NAGPURE <juhinagpure143@gmail.com>
2025-02-07 22:49:04 +0530

    update Navbar component to include logo and navigation button

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -1,7 +1,19 @@
+import assets from "../../assets/assets";
+
 const Navbar = () => {
   return (
     <div>
-      <h1>Navbar</h1>
+      <img
+        src={assets.logo}
+        alt="Logo"
+        className="w-28 lg:w-32 cursor-pointer"
+      />
+      <div className="hidden md:flex items-center gap-5 text=gray-500">
+        <div>
+          <button>Become Educator</button>
+        </div>
+      </div>
+      <div></div>
     </div>
   );
 };
