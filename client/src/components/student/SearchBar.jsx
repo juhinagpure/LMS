35ccdbb11a8b10f45ea2f0510316cd30b2402a58
JUHI NAGPURE <juhinagpure143@gmail.com>
2025-02-11 20:26:44 +0530

    add Companies and CoursesSection components to Home, enhance SearchBar functionality

--- client/src/components/student/SearchBar.jsx
+++ client/src/components/student/SearchBar.jsx
@@ -1,14 +1,28 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { assets } from "../../assets/assets";
 
-const SearchBar = () => {
+const SearchBar = (data) => {
+  const navigate = useNavigate();
+  const [input, setInput] = useState(data ? data : "");
+
+  const onSearchHandler = (e) => {
+    e.preventDefault();
+    navigate("/course-list/" + input);
+  };
   return (
-    <form className="max-w-xl w-full md:h-14 h-12 flex items-center bg-white border border-gray-500/20 rounded">
+    <form
+      onSubmit={onSearchHandler}
+      className="max-w-xl w-full md:h-14 h-12 flex items-center bg-white border border-gray-500/20 rounded"
+    >
       <img
         src={assets.search_icon}
         alt="search_icon"
         className="md:w-auto w-10 px-3"
       />
       <input
+        onChange={(e) => setInput(e.target.value)}
+        value={input}
         type="text"
         placeholder="Search for courses"
         className="w-full h-full outline-none text-gray-500/80"
