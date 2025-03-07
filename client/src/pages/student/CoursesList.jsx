bb2157be8e7283993c3cfa83467e91ba16818748
JUHI NAGPURE <juhinagpure143@gmail.com>
2025-03-07 18:15:09 +0530

    enhance CoursesList component with improved layout and integrated SearchBar

--- client/src/pages/student/CoursesList.jsx
+++ client/src/pages/student/CoursesList.jsx
@@ -1,25 +1,37 @@
 import { useContext } from "react";
+import { useParams } from "react-router-dom";
+import SearchBar from "../../components/student/SearchBar";
 import { AppContext } from "../../context/AppContext"; // Adjust the import path as necessary
-
 const CoursesList = () => {
-  const { navigate } = useContext(AppContext);
+  const { navigate, allCourses } = useContext(AppContext);
+  const { input } = useParams();
 
   return (
     <>
-      <div>
+      <div className="relative md:px-36 px-8 pt-20 text-left w-full">
+        <div className="flex md:flex-row flex-col gap-6 items-start justify-between">
+          <div>
+            <h1 className="text-4xl font-semibold text-gray-800">
+              Course List
+            </h1>
+            <p className="text-gray-500">
+              <span
+                className="text-blue-600 cursor-pointer"
+                onClick={() => {
+                  navigate("/");
+                }}
+              >
+                Home{" "}
+              </span>
+              / <span> Course List</span>
+            </p>
+          </div>
+          <SearchBar data={input} />
+        </div>
         <div>
-          <h1>Course List</h1>
-          <p className="text-gray-500">
-            <span
-              className="text-blue-600 cursor-pointer"
-              onClick={() => {
-                navigate("/");
-              }}
-            >
-              Home{" "}
-            </span>
-            / <span> Course List</span>
-          </p>
+          {allCourses.map((course, index) => (
+            <div key={index}>{course}</div>
+          ))}
         </div>
       </div>
     </>
