34814dc3c96c51ea22fbf46733eabb4c1d323e41
JUHI NAGPURE <juhinagpure143@gmail.com>
2025-04-17 08:03:33 +0530

    enhance MyEnrollments component to display course progress with visual indicators and navigation functionality

--- client/src/pages/student/MyEnrollments.jsx
+++ client/src/pages/student/MyEnrollments.jsx
@@ -1,7 +1,9 @@
+import { Line } from "rc-progress";
 import { useContext, useState } from "react";
 import { AppContext } from "../../context/AppContext";
 const MyEnrollments = () => {
-  const { enrolledCourses, CalculateCourseDuration } = useContext(AppContext);
+  const { enrolledCourses, CalculateCourseDuration, navigate } =
+    useContext(AppContext);
   const [progressArray] = useState([
     { lectureCompleted: 2, totalLectures: 4 },
     { lectureCompleted: 1, totalLectures: 5 },
@@ -31,17 +33,27 @@ const MyEnrollments = () => {
               <th className="px-4 py-3 font-semibold truncate">Status</th>
             </tr>
           </thead>
-          <tbody>
+          <tbody className="text-gray-700">
             {enrolledCourses.map((course, index) => (
-              <tr key={index}>
-                <td>
+              <tr key={index} className="border-b border-gray-500/20">
+                <td className="md:px-4 pl-2 md:pl-4 py-3 flex items-center space-x-3">
                   <img
                     src={course.courseThumbnail}
                     alt=""
                     className="w-14 sm:w-24 md:w-28"
                   />
-                  <div>
-                    <p>{course.courseTitle}</p>
+                  <div className="flex-1">
+                    <p className="mb-1 max-sm:text-sm">{course.courseTitle}</p>
+                    <Line
+                      strokeWidth={2}
+                      percent={
+                        progressArray[index]
+                          ? (progressArray[index].lectureCompleted * 100) /
+                            progressArray[index].totalLectures
+                          : 0
+                      }
+                      className="bg-gray-300"
+                    />
                   </div>
                 </td>
 
@@ -56,8 +68,16 @@ const MyEnrollments = () => {
                 </td>
 
                 <td className="px-4 py-3 max-sm:text-right">
-                  <button className="px-3 sm:px-5 py-1.5 sm:py-2 bg-blue-600 max-sm:text-xs text-white">
-                    On Going
+                  <button
+                    className="px-3 sm:px-5 py-1.5 sm:py-2 bg-blue-600 max-sm:text-xs text-white"
+                    onClick={() => navigate("/player/" + course._id)}
+                  >
+                    {progressArray[index] &&
+                    progressArray[index].lectureCompleted /
+                      progressArray[index].totalLectures ===
+                      1
+                      ? "completed"
+                      : "On Going"}
                   </button>
                 </td>
               </tr>
