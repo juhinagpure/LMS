609462248fb5aee02e7fe5be54ea6334eb40b95a
JUHI NAGPURE <juhinagpure143@gmail.com>
2025-04-18 13:11:33 +0530

    enhance educator layout with Navbar and Sidebar components for improved navigation

--- client/src/components/educator/Navbar.jsx
+++ client/src/components/educator/Navbar.jsx
@@ -1,7 +1,25 @@
+import { UserButton, useUser } from "@clerk/clerk-react";
+import { Link } from "react-router-dom";
+import { assets, dummyEducatorData } from "../../assets/assets";
 const Navbar = () => {
+  // eslint-disable-next-line no-unused-vars
+  const educatorData = dummyEducatorData;
+  const { user } = useUser();
+
   return (
-    <div>
-      <h1>Edu Navbar</h1>
+    <div className="flex items-center justify-between px-4 md:px-8 border-b border-gray-500 py-3">
+      <Link to="/">
+        <img src={assets.logo} alt="Logo" className="w-28 lg:w-32" />
+      </Link>
+
+      <div className="flex items-center gap-5 text-gray-500 relative">
+        <p>Hi! {user ? user.fullName : "Developers"}</p>
+        {user ? (
+          <UserButton />
+        ) : (
+          <img className="max-w-8" src={assets.profile_img} />
+        )}
+      </div>
     </div>
   );
 };
--- client/src/components/educator/Sidebar.jsx
+++ client/src/components/educator/Sidebar.jsx
@@ -1,8 +1,44 @@
+import { useContext } from "react";
+import { NavLink } from "react-router-dom";
+import { assets } from "../../assets/assets";
+import { AppContext } from "../../context/AppContext";
 const Sidebar = () => {
+  const { isEducator } = useContext(AppContext);
+
+  const menuItems = [
+    {
+      name: "Dashboard",
+      path: "/educator",
+      icon: assets.home_icon,
+    },
+    {
+      name: "Add Course",
+      path: "/educator/add-course",
+      icon: assets.add_icon,
+    },
+    {
+      name: "My Courses",
+      path: "/educator/my-courses",
+      icon: assets.my_courses_icon,
+    },
+    {
+      name: "Student Enrolled",
+      path: "/educator/student-enrolled",
+      icon: assets.person_tick_icon,
+    },
+  ];
+
   return (
-    <div>
-      <h1>Sidebar</h1>
-    </div>
+    isEducator && (
+      <div className="md:w-64 w-16 border-r min-h-screen text-base border-gray-500 py-2 flex flex-col">
+        {menuItems.map((item) => {
+          <NavLink>
+            <img src={item.icon} alt="" className="w-6 h-6" />
+            <p className="md:block hidden text-center">{item.name}</p>
+          </NavLink>;
+        })}
+      </div>
+    )
   );
 };
 
